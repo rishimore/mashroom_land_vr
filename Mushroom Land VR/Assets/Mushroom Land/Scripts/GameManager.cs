5e724c0afdb5d9bd163dd7a2c851e5f2f24dd6a7
Rishi More <rishi.more9@gmail.com>
2019-10-13 22:25:58 +0530

    initial commit

---
+++ Mushroom Land VR/Assets/Mushroom Land/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public Transform[] spwanPoints;
+
+	public GameObject player;
+
+	int spwanPointindex = 0;
+
+	bool changePosition;
+
+	// Use this for initialization
+	void Start () {
+		player.transform.position = spwanPoints [0].position;
+	}
+
+	public void OnPointerEnter(){
+		changePosition = true;
+		StartCoroutine ("TransferPlayer");
+	}
+
+	public void OnPointerExit(){
+		changePosition = false;
+		StopCoroutine ("TransferPlayer");
+	}
+
+	IEnumerator TransferPlayer(){
+
+		yield return new WaitForSeconds (2);
+
+		if (changePosition = true) {
+
+			spwanPointindex = spwanPointindex + 1;
+
+			if (spwanPointindex >= spwanPoints.Length) {
+				spwanPointindex = 0;
+			}
+				
+			player.transform.position = spwanPoints [spwanPointindex].position;
+			Debug.Log ("change new spwanpoint" + spwanPointindex);
+		}
+	}
+}
